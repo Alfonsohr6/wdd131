d642d7efed78b42fa170fcf1028458918893ab41
Alfonsohr6 <aioyo.hr@gmail.com>
2025-01-11 23:29:56 -0600

    1 week 7

---
+++ scripts/getdates.js
@@ -0,0 +1,9 @@
+
+// intento conseguir el año 
+const year = document.querySelector("#currentyear");
+
+const today = new Date();
+
+year.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear()}</span>`;
+
+//

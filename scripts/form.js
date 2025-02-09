6894cf17f49c6a794f61dccef9609dda9bc89efb
Alfonsohr6 <aioyo.hr@gmail.com>
2025-02-09 03:57:48 -0600

    5 week 3

--- scripts/form.js
+++ scripts/form.js
@@ -1,4 +1,6 @@
-//here the new project
+//here the new project So sorry the a lot comments but I need to know what myself was thinking to do this parts and 
+//understand better my code and so sorry that I type that in Spanish but I need understand me, I use and apply
+//the code that I understand by videos of Youtube in Spanish so sorry my issues
 
 window.onload = function(){
     // intento conseguir el año 

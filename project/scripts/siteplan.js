06cf1904d676e972a969b9115e5f9858b257bb52
Alfonsohr6 <aioyo.hr@gmail.com>
2025-02-13 20:48:34 -0600

    6 Week 1

---
+++ project/scripts/siteplan.js
@@ -0,0 +1,12 @@
+window.onload = function(){
+    // intento conseguir el año 
+    const currentYear = new Date().getFullYear();
+    document.getElementById("currentYear").innerHTML = currentYear;
+    
+    //Ultima modificacion.
+    
+    const lastModified = document.lastModified;
+    document.getElementById("lastModified").innerHTML = "Last upated: " + lastModified;
+    
+   
+}
